1dd9cbfe7565fa1ab68cef09636dbbf98b286b64
DIR\ilja.volinskis <email>
2026-05-14 12:50:53 +0300

    Add loading, error screens and state

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -22,10 +22,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.marsphotos.network.MarsApi
 import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface MarsUiState {
+    data class Success(val photos: String) : MarsUiState
+    object Error : MarsUiState
+    object Loading : MarsUiState
+}
 
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
     /**
@@ -41,8 +48,12 @@ class MarsViewModel : ViewModel() {
      */
     fun getMarsPhotos() {
         viewModelScope.launch {
-            val photosResult = MarsApi.retrofitService.getPhotos()
-            marsUiState = photosResult
+            marsUiState = try {
+                val photosResult = MarsApi.retrofitService.getPhotos()
+                MarsUiState.Success(photosResult)
+            } catch (e: IOException) {
+                MarsUiState.Error
+            }
         }
     }
 }
